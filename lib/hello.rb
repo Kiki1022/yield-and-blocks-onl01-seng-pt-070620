3cd12bef95550c3149dd4a2d4fbdc6da56d9ee14
Kinnari Shah <kinnari1022@yahoo.com>
2020-07-15 02:38:06 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,11 +1,11 @@
-def hello_t()
-  i = 0 
+# def hello_t()
+#   i = 0 
   
-  while i < array.length 
-  i = i + 1 
-end
+#   while i < array.length 
+#   i = i + 1 
+# end
 
-end
+# end
 
 # call your method here!
 
